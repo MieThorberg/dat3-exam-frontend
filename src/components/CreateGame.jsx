49969f389fc56db840d77140e76f9b4379030374
Ruaffu <78867945+Ruaffu@users.noreply.github.com>
2022-05-09 09:47:08 +0200

    pin generator implementation qapla

--- src/components/CreateGame.jsx
+++ src/components/CreateGame.jsx
@@ -1,82 +1,96 @@
-import React, {useState, useEffect} from 'react'
-import { useNavigate, NavLink } from 'react-router-dom'
-import facade from '../apiFacade'
-import "../styles/Join.css"
+import React, { useState, useEffect } from "react";
+import { useNavigate, NavLink } from "react-router-dom";
+import facade from "../apiFacade";
+import "../styles/Join.css";
 
-const CreateGame = ({ loggedIn}) => {
-  loggedIn = true
+const CreateGame = ({ loggedIn }) => {
+  loggedIn = true;
   let navigate = useNavigate();
 
-  const [error, setError] = useState("")
-  const [data, setData] = useState({ name:"", room:""})
+  const [error, setError] = useState("");
+  const [data, setData] = useState({ name: "", room: "" });
   const [pin, setPin] = useState("");
-  const [game, setGame] = useState('')
+  const [game, setGame] = useState("");
 
-  const handleChange = e => {
-      setData({
-          ...data, 
-          [e.target.name]: e.target.value
-      })
-      
-  }
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const validation = () => {
-      if(!data.name){ 
-          setError("Please enter your name.")
-          return false
-      }
-    //   if(!data.room){ 
+    if (!data.name) {
+      setError("Please enter your name.");
+      return false;
+    }
+    //   if(!data.room){
     //       setError("Please select room.")
     //       return false
-    //   } 
-      setError("")
-      return true
-  }
+    //   }
+    setError("");
+    return true;
+  };
 
-  const handleSubmit = e => {
-      e.preventDefault()
-      const isValid = validation()
-      if(isValid){
-          navigate(`/chat/${data.room}`, { state: data });
-          facade.createGame('user','test123').then(data => setGame(data))
-      }
-
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validation();
+    if (isValid) {
+      navigate(`/chat/${data.room}`, { state: data });
+      facade.createGame("user", "test123").then((data) => setGame(data));
+    }
+  };
 
   const generatePin = () => {
-    const newPin = Math.floor(100000 + Math.random() * 900000).toString().substring(1);
-    setPin(newPin)
-    return setData({...data, room: newPin})
-}
+    const newPin = Math.floor(100000 + Math.random() * 900000)
+      .toString()
+      .substring(1);
+    // setPin(newPin)
+    return setData({ ...data, room: newPin });
+  };
 
-console.log('room:'+data.room);
-console.log('name:'+data.name);
+  console.log("room:" + data.room);
+  console.log("name:" + data.name);
   return (
-      <div>
-          {loggedIn?
-          <form className='form-box center-text' onSubmit={handleSubmit}>
-              <div>
-                  <h2>Create game</h2>
-              </div>
-              <div >
-                  <input type="name" className='joinInput' name="name" placeholder="Game name" onChange={handleChange} />
-              </div>
-              <div>
-              <input type="name" className='joinInput' name="room" placeholder="Enter pin" value={pin} onChange={handleChange}  /><br></br>
-              <input type='button' onClick={generatePin} value="Generate"></input>
-
-              </div>
-              <button  type="submit">Create game</button>
-              {error ? <small>{error}</small> : "" }
-          </form>
-          :
+    <div>
+      {loggedIn ? (
+        <form className="form-box center-text" onSubmit={handleSubmit}>
           <div>
-            <h3>Please Login</h3>
-     <NavLink to="/login">Login</NavLink>
+            <h2>Create game</h2>
           </div>
-}
-      </div>
-  )
-}
+          <div>
+            <input
+              type="name"
+              className="joinInput"
+              name="name"
+              placeholder="Game name"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <input
+              readOnly
+              type="name"
+              className="joinInput"
+              name="room"
+              placeholder="Enter pin"
+              value={data.room}
+              onChange={handleChange}
+            />
+            <br></br>
+            <input type="button" onClick={generatePin} value="Generate"></input>
+          </div>
+          <button type="submit">Create game</button>
+          {error ? <small>{error}</small> : ""}
+        </form>
+      ) : (
+        <div>
+          <h3>Please Login</h3>
+          <NavLink to="/login">Login</NavLink>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default CreateGame
+export default CreateGame;
--- src/components/pages/GameSettingsPage.jsx
+++ src/components/pages/GameSettingsPage.jsx
@@ -1,92 +1,117 @@
-import React from 'react'
-import "../../styles/App.css"
-import { useState, useEffect } from 'react'
-import { useNavigate, NavLink } from 'react-router-dom'
-import facade from '../../apiFacade'
+import React from "react";
+import "../../styles/App.css";
+import { useState, useEffect } from "react";
+import { useNavigate, NavLink } from "react-router-dom";
+import facade from "../../apiFacade";
 
 const GameSettingsPage = ({ mode, setHeadline }) => {
-    //title i topnav
-    useEffect(() => {
-        setHeadline("Game settings");
-    }, []);
+  //title i topnav
+  useEffect(() => {
+    setHeadline("Game settings");
+  }, []);
 
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const [error, setError] = useState("")
-    const [data, setData] = useState({ name: "", room: "" })
-    const [pin, setPin] = useState("");
-    const [game, setGame] = useState('')
+  const [error, setError] = useState("");
+  const [data, setData] = useState({ name: "", room: "" });
+  const [pin, setPin] = useState("");
+  const [game, setGame] = useState("");
 
-    const handleChange = e => {
-        setData({
-            ...data,
-            [e.target.name]: e.target.value
-        })
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
 
+  const validation = () => {
+    if (!data.name) {
+      setError("Please enter your name.");
+      return false;
     }
+    // if (!data.room) {
+    //     setError("Please enter pin code.")
+    //     return false
+    // }
+    setError("");
+    return true;
+  };
 
-    const validation = () => {
-        if (!data.name) {
-            setError("Please enter your name.")
-            return false
-        }
-        if (!data.room) {
-            setError("Please enter pin code.")
-            return false
-        }
-        setError("")
-        return true
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validation();
+    if (isValid) {
+      navigate(`/join_game/${data.room}`, { state: data });
+      facade.createGame("user", "test123").then((data) => setGame(data));
     }
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault()
-        const isValid = validation()
-        if (isValid) {
-            navigate(`/join_game/${data.room}`, { state: data });
-            facade.createGame('user', 'test123').then(data => setGame(data))
-        }
+  const generatePin = (e) => {
+    e.preventDefault();
+    const newPin = Math.floor(100000 + Math.random() * 900000)
+      .toString()
+      .substring(1);
+    // setPin(newPin)
+    return setData({ ...data, room: newPin });
+  };
+  return (
+    <>
+      <div className="background-container">
+        <div
+          id="background-img"
+          style={{ backgroundImage: `url(${mode.image})` }}
+        ></div>
+        <div
+          id="background-img-blur"
+          style={{ backgroundColor: `${mode.blur}` }}
+        ></div>
+      </div>
 
-    }
-
-    const generatePin = () => {
-        const newPin = Math.floor(100000 + Math.random() * 900000).toString().substring(1);
-        // setPin(newPin)
-        return setData({...data, room: newPin})
-    }
-    return (
-
-        <>
-            <div className='background-container'>
-                <div id='background-img' style={{ backgroundImage: `url(${mode.image})` }}></div>
-                <div id='background-img-blur' style={{ backgroundColor: `${mode.blur}` }}></div>
+      <div className="main">
+        <div className="main-container">
+          <div style={{ gridTemplateRows: "auto" }}></div>
+          <div className="section" style={{ gridTemplateRows: "auto 90%" }}>
+            <div
+              className="header"
+              style={{ justifyContent: "end", paddingBottom: "20px" }}
+            >
+              <h1>Game settings</h1>
             </div>
-
-            <div className='main'>
-                <div className='main-container'>
-                    <div style={{ gridTemplateRows: "auto" }}>
-                    </div>
-                    <div className='section' style={{ gridTemplateRows: "auto 90%" }}>
-
-                        <div className='header' style={{ justifyContent: "end", paddingBottom: "20px" }}>
-                            <h1>Game settings</h1>
-                        </div>
-                        <div className='content' style={{ justifyContent: "start", gridTemplateRows: "100%" }}>
-
-                            <form onSubmit={handleSubmit}>
-                                <input readOnly type="name" name="room" placeholder="Generate pin" value={data.room}  onChange={handleChange} /><br></br>
-                                <button onClick={generatePin}>Generate pin</button>
-                                <button type="submit">Enter</button>
-                                {/* If you dont have type in values for the inputs */}
-                                {error ? <small>{error}</small> : ""}
-                            </form>
-
-
-                        </div>
-                    </div>
+            <div
+              className="content"
+              style={{ justifyContent: "start", gridTemplateRows: "100%" }}
+            >
+              <form >
+                <div>
+                  <input
+                    type="name"
+                    name="name"
+                    placeholder="Game name"
+                    onChange={handleChange}
+                  />
                 </div>
+                <div>
+                  <input
+                    readOnly
+                    type="name"
+                    name="room"
+                    placeholder="Generate pin"
+                    value={data.room}
+                    onChange={handleChange}
+                  />
+                  <br></br>
+                  <button onClick={generatePin}>Generate pin</button>
+                </div>
+                <button onClick={handleSubmit}>Enter</button>
+                {/* If you dont have type in values for the inputs */}
+                {error ? <small>{error}</small> : ""}
+              </form>
             </div>
-        </>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default GameSettingsPage;
