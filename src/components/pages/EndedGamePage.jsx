c96bec8d2023a86c02c06cb5073570b9696b2a48
LeoNielsen <leo.nielsen21@gmail.com>
2022-05-16 13:32:33 +0200

    ended game screen with winners

--- src/components/pages/EndedGamePage.jsx
+++ src/components/pages/EndedGamePage.jsx
@@ -2,20 +2,47 @@ import React from 'react'
 import "../../styles/App.css"
 import { useEffect, useState } from 'react'
 import gameController from '../../gameController'
+import { useNavigate, useLocation } from 'react-router-dom'
 import facade from '../../apiFacade'
 
 const EndedGamePage = ({ mode, winners }) => {
+
+    const location = useLocation()
+
     const [day, setDay] = useState("");
     const [data, setData] = useState({});
+    const [players, setPlayers] = useState([]);
+    const [winner, setWinner] = useState("");
+
+    useEffect(() => {
+        setData(location.state)
+    }, [location])
 
     useEffect(() => {
         if (facade.getToken() == undefined) {
-          navigate("/login")
+            navigate("/login")
         }
-      }, [])
+    }, [])
 
-    function getDay() {
-        return gameController.getDay(2).then(data => setDay(data));
+    useEffect(() => {
+        if (data.gameid != undefined) {
+            facade.getWereWolves(data.gameid).then(data => {
+                if (data.length == 0) {
+                    setWinner("Villagers!")
+                } else {
+                    setWinner("Werewolves!")
+                }
+            })
+            facade.getPlayers(data.gameid).then(data => setPlayers(data));
+        }
+    }, [data])
+
+    function getWinnerRole() {
+        if(winner == "Werewolves!"){
+           return "werewolf"
+        } else {
+           return "villager"
+        }
     }
 
     //TODO: make a function to get the winners of the game wololo
@@ -33,52 +60,37 @@ const EndedGamePage = ({ mode, winners }) => {
                     <div className='section' style={{ gridTemplateRows: "40% auto" }}>
 
                         <div className='header' style={{ justifyContent: "end", paddingBottom: "20px" }}>
-                            <h1>Winners wololo</h1>
-                        </div>
-                        <div className='content' style={{ justifyContent: "start", gridTemplateRows: "60% auto" }}>
-
-
-                            <div className='list-grid'>
-
-                                {/* {players.map((player) => {
-                                    return <div key={player.id}>
-                                    <div>
-                                        <img className="profile-img" />
-                                        <h3 style={{ color: 'white' }}>{player.username}</h3>
-                                        </div>
-                                    </div>
-                                })} */}
-
-
-                                <div>
-                                    <div>
-                                        <img className="profile-img" />
-                                        <h3 style={{ color: 'white' }}>wololo</h3>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div>
-                                        <img className="profile-img" />
-                                        <h3 style={{ color: 'white' }}>wololo</h3>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div>
-                                        <img className="profile-img" />
-                                        <h3 style={{ color: 'white' }}>wololo</h3>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div>
-                                        <img className="profile-img" />
-                                        <h3 style={{ color: 'white' }}>wololo</h3>
-                                    </div>
-                                </div>
-
+                            <h1>Winners</h1>
+                            <div className='wololo'>
+                                <h2>{winner}</h2>
                             </div>
                         </div>
-                        {/* <!-- Column 3 (empty) --> */}
-                        <div></div>
+                        <div className='content' style={{ justifyContent: "start", gridTemplateRows: "60% auto" }}>
+                            <div className='joined-players-section'>
+                                <div className='joined-players-scroll'>
+
+                                    <div className='list-grid' id="playerlist">
+
+                                        {
+                                            
+
+                                            (players.map((player) => {
+                                                if(player.characterName == getWinnerRole())
+                                                return <div key={player.id}>
+                                                    <div className='vote'>
+                                                        <img id={player.id} className="profile-img" /> {/* REMEMBER! set active on one player, or else the active vote will not show  */}
+                                                        <h3 style={{ color: 'white' }}>{player.username}</h3>
+                                                    </div>
+                                                </div>
+                                            }))
+
+                                        }
+                                    </div>
+                                </div>
+                                {/* <!-- Column 3 (empty) --> */}
+                                <div></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
