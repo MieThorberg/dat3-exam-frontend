082ce76dac9156fc2398d277178de08922485b1f
Ruaffu <78867945+Ruaffu@users.noreply.github.com>
2022-05-16 11:04:00 +0200

    seetings change

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 // todo: chance url
-/* const URL = "http://localhost:8080/ex_startcode_backend_war_exploded";  */
-const URL = "http://localhost:8080/werewolf_war_exploded";
+ const URL = "http://localhost:8080/ex_startcode_backend_war_exploded";  
+// const URL = "http://localhost:8080/werewolf_war_exploded";
 // const URL = "https://luksuspanda.live/werewolf"
 
  
