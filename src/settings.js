dba432809fb98ae293c2f3a8187340454a8144f9
MieThorberg <cph-mt373@cphbusiness.dk>
2022-06-14 09:42:13 +0200

    change url

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
-/* const URL = "https://miemt.me/exam-backend"; */
-const URL = "http://localhost:8080/exam_backend_war_exploded"
+const URL = "https://miemt.me/exam-backend";
+/* const URL = "http://localhost:8080/exam_backend_war_exploded" */
 
 export default URL;
